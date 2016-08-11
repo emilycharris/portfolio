eceef96c09fba1e0afc6f499c271e383d62441e9
emilycharris <emily.harris.mail@gmail.com>
2016-08-11 18:34:40 -0400

    rearranging sections, portfolio 2 ready

--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class FamilyTvView(TemplateView):
+    template_name = 'familytv.html'
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from main.views import IndexView
+from main.views import IndexView, FamilyTvView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', IndexView.as_view(), name='index_view')
+    url(r'^$', IndexView.as_view(), name='index_view'),
+    url(r'^familytv/$', FamilyTvView.as_view(), name='family_tv_view'),
 ]
